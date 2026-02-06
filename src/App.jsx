7b72a1ba1c639493880ce51cb01ee8d19ab81a9b
alexa neff <alexanef@buffalo.edu>
2026-02-05 19:58:29 -0500

    added state control, handle current todo input

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,50 @@
-import "./styles.css"
+import { useState } from "react";
+import "./styles.css";
 
 export default function App() {
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e){
+    e.preventDefault()
+
+    setTodos((currentTodos) => {
+      return [...currentTodos, {id: crypto.randomUUID(), title: newItem, completed: false}]
+    })
+  }
+
   return (
     <>
-  
-   <form className="new-item-form">
-    <div className="form-row">
-      <label htmlFor="item">New Item</label>
-      <input type="text" id="item" />
-    </div>
+      <form onSubmit={handleSubmit} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input value= { newItem }
+          onChange={e => setNewItem(e.target.value)}
+          type="text" 
+          id="item" />
+        </div>
 
-    <button className="button">Add Item</button>
-  </form>
+        <button className="button">Add Item</button>
+      </form>
 
-  <h1 className="header"> TO DO LIST</h1>
-</>
-)
+      <h1 className="header"> TO DO LIST</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 2
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  );
 }
